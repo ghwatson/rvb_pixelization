cc8331cf8463a9dd814ccdb8a93873ad9d37c16f
ghwatson <grant.watson.1991@gmail.com>
2013-10-26 20:06:48 -0400

    Add odd diameter function

--- bresenham.py
+++ bresenham.py
@@ -25,7 +25,7 @@ def get_horizontal_line(begin,end):
 # of the Bresenham circle algorithm:
 #     http://en.wikipedia.org/wiki/Midpoint_circle_algorithm
 # The ordering of the points in the set is like so:
-#          13 11 12   
+#          13 12 11   
 #       10  9  8  7  6
 #        5  4  3  2  1
 #       18 17 16 15 14
@@ -65,4 +65,51 @@ def raster_disk(x0,y0,radius):
         
         coords = coords + line1 + line2 + line3 + line4
 
+    return coords
+
+# This algorithm is capable of dealing with half-integer origin and integer
+# radii, unlike raster_disk which can only deal with integer origin and integer
+# radii.
+# TODO: 
+# -Get rid of the int conversion hack by redesigning stuff as needed.
+def raster_disk_offsite_integer_rad(x0,y0,radius):
+    x = 0.5 # Start with the rightmost of the 2 upper pixels.
+    y = radius
+    
+    coords = []
+    
+    f = 1 - radius
+    ddF_x = 1
+    ddF_y = -2 * radius
+    
+    
+    while (y >= x):
+        #TODO: This conversion is pretty anti-pythonic.
+        x1 = int(x0+x)
+        x2 = int(x0-x)
+        x3 = int(x0 + y)
+        x4 = int(x0 - y)
+        y1 = int(y0 + y)
+        y2 = int(y0 - y)
+        y3 = int(y0 + x)
+        y4 = int(y0 - x)
+        
+        line1 = get_horizontal_line((x1, y1), (x2, y1))
+        line2 = get_horizontal_line((x1, y2), (x2, y2))
+        line3 = get_horizontal_line((x3, y3), (x4, y3))
+        line4 = get_horizontal_line((x3, y4), (x4, y4))
+         
+        coords = coords + line1 + line2 + line3 + line4
+        
+#         ddF_x == 2 * x + 1
+#         ddF_y == -2 * y
+#         f == x*x + y*y - radius*radius + 2*x - y + 1
+        if f >= 0:
+            y -= 1
+            ddF_y += 2
+            f += ddF_y
+        x += 1
+        ddF_x += 2
+        f += ddF_x    
+        
     return coords
